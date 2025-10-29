5a2caafbd11369a27ec82a0ffca8744152299d69
neinzz246 <email_of_neinzz246@gmail.com>
2025-10-29 20:42:14 +0530

    Added root route for backend

--- backend/server.js
+++ backend/server.js
@@ -57,6 +57,8 @@ ${message}
     res.status(500).json({ success: false, msg: "Failed to send email." });
   }
 });
-
+app.get("/", (req, res) => {
+  res.send("🌸 Nikki’s Bouquet backend is live and working!");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
